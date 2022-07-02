595bbc702518d4247391672c2663d913dbb1190c
oskar-anderson <andersonkarloskar@gmail.com>
2022-07-02 18:49:57 +0300

    test v0.0.6

--- dev_tools.py
+++ dev_tools.py
@@ -1,70 +1,78 @@
+from cmath import e
 import sys
 import re
 import getopt
+from tkinter import E
 
+TEMPLATE_FILE_PATHS = [ './wwwroot/index-template.html' ]
+BUILD_FILE_PATHS =    [ './wwwroot/index.html' ]
 
 def setLastCommitHashToIndexFile(argv):
     # constants
     ejs_pattern = "(<%=\s([a-zA-Z\-\_]+)\s%>)"
-    template_file_path = './wwwroot/index-template.html'
-    build_file_path =    './wwwroot/index.html'
-    
+    if len(TEMPLATE_FILE_PATHS) != len(BUILD_FILE_PATHS):
+        raise Exception("Invalid paths")
+
     # terminal argument handling
-    arg_help = "{0} -h <help> <hash> <version>".format(argv[0])
+    arg_help = "{0} -h <help> <hash> <commit-msg>".format(argv[0])
     if (len(argv) == 1):
-        print("No arguments passed to script")
-        sys.exit(2)
+        sys.exit("No arguments passed to script")
     commit_hash = ""
-    version = ""
+    commit_msg = ""
+
     try:
-        opts, args = getopt.getopt(argv[1:], "h", ["help", "hash=", "version="])
+        opts, args = getopt.getopt(argv[1:], "h", ["help", "hash=", "commit-msg="])
         if (len(args) != 0):
-            print(f"Unknown arguments passed {args}")
-            sys.exit(2)
-    except:
-        print("Unknown command try: --help")
-        sys.exit(2)
+            sys.exit(f"Unknown arguments passed {args}")
+    except getopt.GetoptError as err:
+        sys.exit(f"Unknown command try: --help")
     for opt, arg in opts:
         if opt in ("-h", "--help"):
             print(arg_help)
-            sys.exit(2)
+            return
         elif opt in ("--hash"):
             commit_hash = arg
-        elif opt in ("--version"):
-            version = arg
+        elif opt in ("--commit-msg"):
+            commit_msg = arg
 
     # function body
-    template_file_content = ""    
-    with open(template_file_path) as f:
-        template_file_content = f.read()
-    
-    print("🧊🧊🧊")
-    print("input string:\n", template_file_content)
-    print("🧊🧊🧊\n")
+    for i in range(len(TEMPLATE_FILE_PATHS)):
+        TEMPLATE_FILE_PATH = TEMPLATE_FILE_PATHS[i]
+        BUILD_FILE_PATH = BUILD_FILE_PATHS[i]
+        template_file_content = ""    
+        with open(TEMPLATE_FILE_PATH) as f:
+            template_file_content = f.read()
+        
+        print("🧊🧊🧊")
+        print("input string:\n", template_file_content)
+        print("🧊🧊🧊\n")
 
-    print()
-    matches = re.findall(ejs_pattern, template_file_content)
-    if (len(matches) == 0):
-        print("No matches found!")
-        sys.exit(2)
-    for x in matches:
-        key_in_templating_syntax = x[0] # Example value: <%= latest-version %>
-        key = x[1]                      # Example value: latest-version
-        if (not key):
-            raise RuntimeError("Match pattern does not have 2 groups: ", x)
-        if (key == "commit-hash"):
-            print(f"replacing: {key} with '{commit_hash}'")
-            template_file_content = template_file_content.replace(key_in_templating_syntax, commit_hash)
-        if (key == "latest-version"):
-            print(f"replacing: {key} with '{version}'")
-            template_file_content = template_file_content.replace(key_in_templating_syntax, version)
-    
-    print("🔥🔥🔥")
-    print("output string:\n", template_file_content)
-    print("🔥🔥🔥")
-    with open(build_file_path, 'w') as f:
-        f.write(template_file_content)
+        matches = re.findall(ejs_pattern, template_file_content)
+        if (len(matches) == 0):
+            print(f"No matches found in {TEMPLATE_FILE_PATH}")
+        for x in matches:
+            key_in_templating_syntax = x[0] # Example value: <%= latest-version %>
+            key = x[1]                      # Example value: latest-version
+            if (not key):
+                raise SystemError("Match pattern does not have 2 groups: ", x)
+            if (key == "commit-hash"):
+                print(f"replacing: {key} with '{commit_hash}'")
+                template_file_content = template_file_content.replace(key_in_templating_syntax, commit_hash)
+            if (key == "latest-version"):
+                pattern = "(v\d+\.\d+\.\d+)"
+                r = re.search(pattern, commit_msg)
+                if not r:
+                    print(f"latest-version value does not match expected pattern. Regex: '{pattern}', example 'v1.6.13'.")
+                    continue
+                version = r.group(0)
+                print(f"replacing: {key} with '{version}'")
+                template_file_content = template_file_content.replace(key_in_templating_syntax, version)
+        
+        print("🔥🔥🔥")
+        print("output string:\n", template_file_content)
+        print("🔥🔥🔥")
+        with open(BUILD_FILE_PATH, 'w') as f:
+            f.write(template_file_content)
 
-  
 
 setLastCommitHashToIndexFile(sys.argv)
